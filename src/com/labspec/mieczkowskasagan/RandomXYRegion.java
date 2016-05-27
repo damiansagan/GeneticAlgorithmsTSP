10852a94afe954fb1f5151a1422a6870446a04c1
Damian Sagan <colofsun@gmail.com>
2016-05-27 13:07:23 +0200

    comments added in empty class

--- src/com/labspec/mieczkowskasagan/RandomXYRegion.java
+++ src/com/labspec/mieczkowskasagan/RandomXYRegion.java
@@ -1,4 +1,29 @@
 package com.labspec.mieczkowskasagan;
 
+import java.awt.*;
+
 public class RandomXYRegion {
+
+    /*
+    proponuję zrobić sobie strukturę danych zawierającą punkty,
+    na przykład listę losowych punktow i to będą nasze miasta,
+    a potem łatwo będzie używać wbudowanych metod :)
+
+    trzeba rozszerzyć klasę Region tak jak w RandomDistancesRegion
+    WAŻNE: na zewnątrz ma być nie widać, że to jest zaimplementowane jako Point,
+    czyli zgodnie z sygnaturami klasy bazowej :)
+    public abstract int getDistanceBetween(int cityA, int cityB);
+    public abstract Integer getNearestCityFrom(int city);
+
+    zrob tez metode get city position: Point getPosition(int city);
+    przyda się potem podczas rysowania :)
+
+    poniżej przykład:
+    */
+    {
+    Point p1 = new Point(1,2);
+    Point p2 = new Point(3,5);
+    System.out.println("Odleglosc miedzy p1 i p2: "+p1.distanceSq(p2));
+    }
+
 }
